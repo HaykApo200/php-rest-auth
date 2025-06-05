048af5d210dc11e3007cf0b77760b2baa02c7803
HaykApo200 <haykapoyan0103@gmail.com>
2025-06-05 17:25:24 +0400

    add signup page

---
+++ public/js/signup.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('signup-form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const data = {
+        email: formData.get('email'),
+        password: formData.get('password')
+    };
+
+    const response = await fetch('/api/signup.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    const resultData = await response.json();
+    result.innerText = resultData.message || 'Unknown response';
+});
