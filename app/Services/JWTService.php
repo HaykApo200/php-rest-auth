d409d1fd0f712c8019c9311882ec08c79138f55d
HaykApo200 <haykapoyan0103@gmail.com>
2025-06-09 16:31:45 +0400

    Create TokenService

--- app/Services/JWTService.php
+++ app/Services/JWTService.php
@@ -4,3 +4,27 @@ declare(strict_types=1);
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+
+class TokenService
+{
+    private string $secretKey = 'secretKey';
+    private string $alg = 'HS256';
+
+    public function generateToken(string $email): string
+    {
+        $payload = [
+            'iss' => 'http://localhost',
+            'aud' => 'http://localhost',
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'email' => $email
+        ];
+
+        return JWT::encode($payload, $this->secretKey, $this->alg);
+    }
+
+    public function validateToken(string $jwt): object
+    {
+        return JWT::decode($jwt, new Key($this->secretKey, $this->alg));
+    }
+}
